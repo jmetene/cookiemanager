423a4014e4266eb486041d40b85cc5d067638f82
USERSAD\jmetenen <juan.metenenguema@externos.correo.gob.es>
2024-06-28 08:44:39 +0200

    Creation of project structure

---
+++ src/test/java/com/metene/CookiemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
