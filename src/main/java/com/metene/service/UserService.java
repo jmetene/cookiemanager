e17a9d5f276fc6af2e8a6314639acd0ffae942d7
USERSAD\jmetenen <juan.metenenguema@externos.correo.gob.es>
2024-07-18 22:59:09 +0200

    feat(Project): Cambios grandes en el diseño del sistema

--- src/main/java/com/metene/service/UserService.java
+++ src/main/java/com/metene/service/UserService.java
@@ -2,15 +2,7 @@ package com.metene.service;
 
 import com.metene.service.dto.*;
 
-import java.util.List;
-
 public interface UserService {
-    void saveCookies(List<CookieRequest> cookies, String token);
     void updateUser(UserRequest userRequest, String token);
-    void deleteCookie(String token, String cookieName);
-    void saveCookieBanner(String token, CookieBannerRequest banner);
-    List<CookieBannerResponse> getCookieBanners(String token);
-    void updateCookieBanner(String token, CookieBannerRequest banner, Long id) throws NoSuchFieldException;
     UserResponse getUser(String token);
-    CookieBannerResponse getCookieBannerDetail(String token, Long id);
 }
--- src/main/java/com/metene/service/impl/UserServiceImpl.java
+++ src/main/java/com/metene/service/impl/UserServiceImpl.java
@@ -1,25 +1,13 @@
 package com.metene.service.impl;
 
-import com.metene.domain.entity.Cookie;
-import com.metene.domain.entity.CookieBanner;
-import com.metene.domain.entity.CookieStatistics;
 import com.metene.domain.entity.User;
-import com.metene.domain.repository.CookieBannerRepository;
-import com.metene.domain.repository.CookieRepository;
 import com.metene.domain.repository.UserRepository;
 import com.metene.service.JWTService;
 import com.metene.service.UserService;
 import com.metene.service.dto.*;
-import com.metene.service.mapper.CookieBannerMapper;
-import com.metene.service.mapper.CookieMapper;
-import com.metene.service.mapper.CookieStatisticMapper;
 import com.metene.service.mapper.UserMapper;
-import jakarta.persistence.PersistenceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -27,22 +15,6 @@ public class UserServiceImpl implements UserService {
 
     private final JWTService jwtService;
     private final UserRepository userRepository;
-    private final CookieRepository cookieRepository;
-    private final CookieBannerRepository cookieBannerRepository;
-
-    @Override
-    public void saveCookies(List<CookieRequest> cookies, String token) {
-        User user = userRepository.findByUsername(getUserName(token)).orElse(new User());
-        List<Cookie> cookiesToSave =  cookies.stream().map(CookieMapper::toEntity).toList();
-
-        for (int i = 0; i < cookiesToSave.size(); i++) {
-            Cookie cookie = cookiesToSave.get(i);
-            List<CookieStatistics> statistics = CookieStatisticMapper.toEntityList(cookies.get(i).getStatistics());
-            cookie.addStatistics(statistics);
-            user.add(cookie);
-        }
-        userRepository.save(user);
-    }
 
     @Override
     public void updateUser(UserRequest userRequest, String token) {
@@ -55,64 +27,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void deleteCookie(String token, String cookieName) {
-        String username = getUserName(token);
-        User user = userRepository.findByUsername(username).orElseThrow();
-        Cookie cookie = cookieRepository.findByNameAndUser(cookieName, username).orElseThrow();
-
-        user.remove(cookie);
-        userRepository.save(user);
-    }
-
-    @Override
-    public void saveCookieBanner(String token, CookieBannerRequest banner) {
-        String username = getUserName(token);
-
-        User user = userRepository.findByUsername(username).orElseThrow();
-        user.add(CookieBannerMapper.toEntity(banner));
-        userRepository.save(user);
-    }
-
-    @Override
-    public List<CookieBannerResponse> getCookieBanners(String token) {
-        String username = getUserName(token);
-        List<CookieBanner> banners = cookieBannerRepository.findCookieBannerByUserName(username).orElseThrow();
-        return banners.stream().map(CookieBannerMapper::toDto).toList();
-    }
-
-    @Override
-    public void updateCookieBanner(String token, CookieBannerRequest banner, Long id) throws NoSuchFieldException {
-        String username = getUserName(token);
-        User user = userRepository.findByUsername(username).orElseThrow();
-
-        if (cookieBannerRepository.findById(id).isEmpty()) throw new NoSuchFieldException("Banner no encontrado");
-
-        CookieBanner cookieBannerToUpdate = CookieBannerMapper.toEntity(banner);
-        cookieBannerToUpdate.setId(id);
-
-        user.add(cookieBannerToUpdate);
-        userRepository.save(user);
-    }
-
-    @Override
     public UserResponse getUser(String token) {
         return UserMapper.toDTO(userRepository.findByUsername(getUserName(token)).orElseThrow());
-    }
-
-    @Override
-    public CookieBannerResponse getCookieBannerDetail(String token, Long id) {
-        String username = getUserName(token);
-
-        CookieBanner banner;
-        try {
-            banner = cookieBannerRepository.findCookieBannerByIdAndUserName(username, id);
-            if (banner == null) throw new NoSuchElementException("Elemento no encontrado");
-        } catch (NoSuchElementException e) {
-            throw new NoSuchElementException(e);
-        } catch (Exception e) {
-            throw new PersistenceException(e);
-        }
-        return CookieBannerMapper.toDto(banner);
     }
 
     private String getUserName(String token) {
