e9707c06923277627010eea6ee73308f547e48d8
USERSAD\jmetenen <juan.metenenguema@externos.correo.gob.es>
2024-07-24 16:22:38 +0200

    feat(Controller): Fin estadísticas cookies dominio

--- src/main/java/com/metene/service/utiles/StatisticsUtils.java
+++ src/main/java/com/metene/service/utiles/StatisticsUtils.java
@@ -24,15 +24,6 @@ public class StatisticsUtils {
                 .collect(Collectors.joining("_"));
     }
 
-    public static Integer getTotalQueryParams(String estado, String pais, String plataforma, String fechaInicio, String fechaFin) {
-        return Stream
-                .of(fechaInicio != null ? Constantes.START_DATE : null, fechaFin != null ? Constantes.END_DATE : null,
-                        estado != null ? Constantes.STATUS : null, pais != null ? Constantes.COUNTRY : null,
-                        plataforma != null ? Constantes.PLATFORM : null)
-                .filter(Objects::nonNull)
-                .toList().size();
-    }
-
     public static Map<String, String> extractQueryParams(List<Map.Entry<String, String[]>> parametros) {
         Map<String, String> mapOfqueryParamsValue = new HashMap<>();
         parametros.forEach(stringEntry -> mapOfqueryParamsValue.put(stringEntry.getKey(), clean(stringEntry.getValue())));
